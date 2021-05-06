248ae726ed94669fbebe88f509da57fd9eb665fb
alvaroquintana <42539212+alvaroquintana1@users.noreply.github.com>
2021-05-06 21:33:21 +0200

    Update register and login

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from tinydb import TinyDB, Query
+import uuid
 
 app = FastAPI()
 
@@ -9,28 +10,39 @@ selector = Query()
 # Authentication endpoints
 @app.post("/auth/register")
 def auth_register(email, password):
-    encontrado = user_accounts.search(selector.email == str(email))
-    if not encontrado:
-         user_accounts.insert({"email":email, "contrasena":password})
-         return {"msg":"Usuario creado"}  
+    if not email or not password:
+        return{"msg":"No has enviado los datos correctos"}
+    found = user_accounts.search(selector.email == str(email))
+    if not found:
+        user_id = str(uuid.uuid4())
+        user_accounts.insert({"user_id":user_id[:5],"email":email, "password":password})
+        return {"msg":"New user"}  
     else:
-       return  {"msg":"Error ya existe"}
+       return  {"msg":"Error, the user already exists"}
 
 @app.post("/auth/login")
 def auth_login(email, password):
+    if not email or not password:
+        return {"msg":"No has enviado los datos correctos"}
     encontrado = user_accounts.search(selector.email == str(email))
-    if not user_accounts:
+    #email_and_password = user_accounts.search(selector.contrasena == str(password))
+    if not encontrado:
         return{'msg':'el usuario no existe'}
 
-    elif encontrado[0]['contrasena'] != password:
+    elif encontrado[0]['password'] != password:
         return {'msg':'contraseña incorrecta'}
+    else:
+        token = str(uuid.uuid4())
+        user_accounts.update({"token":token}, selector.email == email)
+        return token
+        
 
 # Authenticated endpoints
 @app.post("/users/{user_id}/accounts")
 def create_account(user_id, token, url, username, password):
-    if password not in user_accounts:
-        user_accounts.insert({"url":url, "password":password})
-        return {'msg':'Contraseña creada y url creada'}
+    if user_id not in user_accounts:
+        user_accounts.insert({"user_id":user_id,"url":url,"token":token,"username":username,"password":password})
+        return {'msg':'Contraseña creada'}
                         
     else:
         return{'msg':'Esa contraseña ya existe'}
